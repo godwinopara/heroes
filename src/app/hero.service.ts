1a7d88f494b8ffe6aa90be73935ba9994908ae92
godwin opara <godwinopara62@yahoo.com>
2022-08-22 18:23:03 +0100

    added error 404 if heros id is not found

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -47,6 +47,14 @@ export class HeroService {
       )
   }
 
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
   /** PUT: update the hero on the server */
   updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
